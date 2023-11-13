c413415d0d64ed1f205428eeb208fe39ea256ef5
Laima <pileliene.laima@gmail.com>
2023-11-13 21:07:48 +0200

    font awesome

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { Header } from "./components/header/Header";
 import { Aside } from "./components/aside/Aside";
 import { Main } from "./components/main/Main";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return(
     <>
+
     <Header />
     <Aside />
     <Main />
+    <div className="App">
+    <FontAwesomeIcon icon={faEnvelope} />
+    </div>
     </>
   )
 }
